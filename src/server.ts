a98a3343dd1d2ca0bcb11bd091869568dfabf69f
Mena Mahany <mahanymena@gmail.com>
2025-02-26 01:53:07 +0200

    improved /logs endpoint with helpers function

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { SystemService } from "./services/systemService";
 import logger from "./utils/winston/logger";
 import fs from "fs";
 import path from "path";
+import { convertLogsToJson } from "./utils/helpers";
 
 const app = express();
 const port = 5001;
@@ -33,18 +34,7 @@ app.get("/logs", async (_req: Request, res: Response) => {
     const logs = await fs.promises.readFile(logsDir, "utf-8");
 
     // Convert log file into JSON format
-    const logEntries = logs
-      .split("\n")
-      .filter((line) => line.trim() !== "")
-      .map((line) => {
-        const match = line.match(
-          /(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z) \[(\w+)\]: (.*)/,
-        );
-        if (match) {
-          return { timestamp: match[1], level: match[2], message: match[3] };
-        }
-        return { raw: line };
-      });
+    const logEntries = convertLogsToJson(logs);
 
     res.status(200).json({ logs: logEntries });
   } catch (error: any) {
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,3 +3,20 @@ import path from "path";
 export const getFileName = (filePath: string) => {
   return path.basename(filePath);
 };
+
+export const convertLogsToJson = (logs: string) => {
+  const logEntries = logs
+    .split("\n")
+    .filter((line) => line.trim() !== "")
+    .map((line) => {
+      const match = line.match(
+        /(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z) \[(\w+)\]: (.*)/,
+      );
+      if (match) {
+        return { timestamp: match[1], level: match[2], message: match[3] };
+      }
+      return { raw: line };
+    });
+
+  return logEntries;
+};
