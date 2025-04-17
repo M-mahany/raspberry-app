bb810170c308337546751bc3a4a8fe5ee43025d2
Mena Mahany <mahanymena@gmail.com>
2025-04-17 21:11:22 +0200

    sorted logs from newest first

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -18,6 +18,7 @@ export const convertLogsToJson = (
   const logEntries = logs
     .split("\n")
     .filter((line) => line.trim() !== "")
+    .reverse()
     .map((line) => {
       const match = line.match(
         /(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z) \[(\w+)\]: (.*)/,
