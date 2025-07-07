dcb52d7f5bf71c2d78b941c27e7760bdb03c5fb3
Mena Mahany <mahanymena@gmail.com>
2025-07-07 18:05:29 +0300

    pass mic status on connect - socket

--- src/utils/socket/socketClient.ts
+++ src/utils/socket/socketClient.ts
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 import dotenv from "dotenv";
 import logger from "../winston/logger";
+import { isMicActive } from "../../jobs/audioRecording";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ export let isOnline = false;
 const socket = io(process.env.MAIN_SERVER_URL, {
   query: {
     clientType: "device",
+    micStatus: isMicActive,
     accessToken: process.env.ACCESS_TOKEN,
   },
 });
