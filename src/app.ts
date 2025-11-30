25da810345f93d4248e865b9ecccc51110151d4b
Mena Mahany <mahanymena@gmail.com>
2025-11-30 02:08:40 +0200

    enabled reset password route + reboot

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import "./jobs/autoUpdateCron";
 import fs from "fs";
 import { convertLogsToJson } from "./utils/helpers";
 import "./utils/socket/socketClient";
+import { exec } from "child_process";
 // import { exec } from "child_process";
 
 const app = express();
@@ -74,18 +75,18 @@ app.get("/update-system", async (_req: Request, res: Response) => {
   }
 });
 
-// app.get("/reboot", async (_req: Request, res: Response) => {
-//   try {
-//     SystemService.rebootDevice();
-//     res
-//       .status(200)
-//       .json({ message: "Device will be rebooted in 3 seconds..." });
-//   } catch (error: any) {
-//     res
-//       .status(500)
-//       .json({ message: `Error rebooting device: ${error?.message || error}` });
-//   }
-// });
+app.get("/reboot", async (_req: Request, res: Response) => {
+  try {
+    SystemService.rebootDevice();
+    res
+      .status(200)
+      .json({ message: "Device will be rebooted in 3 seconds..." });
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error rebooting device: ${error?.message || error}` });
+  }
+});
 
 // app.get("/refresh_tailscale", async (_req: Request, res: Response) => {
 //   try {
@@ -99,41 +100,41 @@ app.get("/update-system", async (_req: Request, res: Response) => {
 //   }
 // });
 
-// app.post("/reset_password", (req: Request, res: Response) => {
-//   const { user, password } = req.body;
+app.post("/reset_password", (req: Request, res: Response) => {
+  const { user, password } = req.body;
 
-//   if (!user || !password) {
-//     res.status(400).json({ error: "Missing user or password" });
-//     return;
-//   }
+  if (!user || !password) {
+    res.status(400).json({ error: "Missing user or password" });
+    return;
+  }
 
-//   if (!/^[a-z_][a-z0-9_-]*[$]?$/.test(user)) {
-//     res.status(400).json({ error: "Invalid username format" });
-//     return;
-//   }
+  if (!/^[a-z_][a-z0-9_-]*[$]?$/.test(user)) {
+    res.status(400).json({ error: "Invalid username format" });
+    return;
+  }
 
-//   if (
-//     typeof password !== "string" ||
-//     password.length < 6 ||
-//     password.includes("'")
-//   ) {
-//     res.status(400).json({ error: "Weak or unsafe password format" });
-//     return;
-//   }
+  if (
+    typeof password !== "string" ||
+    password.length < 6 ||
+    password.includes("'")
+  ) {
+    res.status(400).json({ error: "Weak or unsafe password format" });
+    return;
+  }
 
-//   const command = `echo '${user}:${password}' | sudo /usr/sbin/chpasswd`;
+  const command = `echo '${user}:${password}' | sudo /usr/sbin/chpasswd`;
 
-//   exec(command, (error, _stdout, stderr) => {
-//     if (error) {
-//       console.error("Password reset failed:", stderr);
-//       res.status(500).json({ error: stderr.trim() || "Command failed" });
-//       return;
-//     }
+  exec(command, (error, _stdout, stderr) => {
+    if (error) {
+      console.error("Password reset failed:", stderr);
+      res.status(500).json({ error: stderr.trim() || "Command failed" });
+      return;
+    }
 
-//     res.json({ message: "Password reset successfully" });
-//     return;
-//   });
-// });
+    res.json({ message: "Password reset successfully" });
+    return;
+  });
+});
 
 app.listen(port, () => {
   logger.info(`🚀 Raspberry app listening on port ${port}`);
