321588de17527136c5fa85dc49612166130173d0
Mena Mahany <mahanymena@gmail.com>
2025-11-30 02:59:15 +0200

    commented endpoints for security

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ import "./jobs/autoUpdateCron";
 import fs from "fs";
 import { convertLogsToJson } from "./utils/helpers";
 import "./utils/socket/socketClient";
-import { exec } from "child_process";
 // import { exec } from "child_process";
 
 const app = express();
@@ -100,63 +99,63 @@ app.get("/reboot", async (_req: Request, res: Response) => {
 //   }
 // });
 
-app.post("/reset_password", (req: Request, res: Response) => {
-  const { user, password } = req.body;
+// app.post("/reset_password", (req: Request, res: Response) => {
+//   const { user, password } = req.body;
 
-  if (!user || !password) {
-    res.status(400).json({ error: "Missing user or password" });
-    return;
-  }
+//   if (!user || !password) {
+//     res.status(400).json({ error: "Missing user or password" });
+//     return;
+//   }
 
-  if (!/^[a-z_][a-z0-9_-]*[$]?$/.test(user)) {
-    res.status(400).json({ error: "Invalid username format" });
-    return;
-  }
+//   if (!/^[a-z_][a-z0-9_-]*[$]?$/.test(user)) {
+//     res.status(400).json({ error: "Invalid username format" });
+//     return;
+//   }
 
-  if (
-    typeof password !== "string" ||
-    password.length < 6 ||
-    password.includes("'")
-  ) {
-    res.status(400).json({ error: "Weak or unsafe password format" });
-    return;
-  }
+//   if (
+//     typeof password !== "string" ||
+//     password.length < 6 ||
+//     password.includes("'")
+//   ) {
+//     res.status(400).json({ error: "Weak or unsafe password format" });
+//     return;
+//   }
 
-  const command = `echo '${user}:${password}' | sudo /usr/sbin/chpasswd`;
+//   const command = `echo '${user}:${password}' | sudo /usr/sbin/chpasswd`;
 
-  exec(command, (error, _stdout, stderr) => {
-    if (error) {
-      console.error("Password reset failed:", stderr);
-      res.status(500).json({ error: stderr.trim() || "Command failed" });
-      return;
-    }
+//   exec(command, (error, _stdout, stderr) => {
+//     if (error) {
+//       console.error("Password reset failed:", stderr);
+//       res.status(500).json({ error: stderr.trim() || "Command failed" });
+//       return;
+//     }
 
-    res.json({ message: "Password reset successfully" });
-    return;
-  });
-});
+//     res.json({ message: "Password reset successfully" });
+//     return;
+//   });
+// });
 
-app.get("/current_user", (_req: Request, res: Response) => {
-  exec("whoami", (error, stdout, stderr) => {
-    if (error) {
-      console.error("Error getting username:", stderr);
-      res.status(500).json({ error: stderr.trim() || "Command failed" });
-      return;
-    }
-    res.json({ username: stdout.trim() });
-  });
-});
+// app.get("/current_user", (_req: Request, res: Response) => {
+//   exec("whoami", (error, stdout, stderr) => {
+//     if (error) {
+//       console.error("Error getting username:", stderr);
+//       res.status(500).json({ error: stderr.trim() || "Command failed" });
+//       return;
+//     }
+//     res.json({ username: stdout.trim() });
+//   });
+// });
 
 
-app.get("/check_ssh", (_req, res) => {
-  exec("nc -zv 127.0.0.1 22", (err, stdout, stderr) => {
-    if (err) {
-      res.json({ ssh: false, info: stderr.trim() });
-      return;
-    }
-    res.json({ ssh: true, info: stdout.trim() });
-  });
-});
+// app.get("/check_ssh", (_req, res) => {
+//   exec("nc -zv 127.0.0.1 22", (err, stdout, stderr) => {
+//     if (err) {
+//       res.json({ ssh: false, info: stderr.trim() });
+//       return;
+//     }
+//     res.json({ ssh: true, info: stdout.trim() });
+//   });
+// });
 
 app.listen(port, () => {
   logger.info(`🚀 Raspberry app listening on port ${port}`);
